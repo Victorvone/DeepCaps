c69fcd4cb05a68afe17d6e053499168971193fc2
Victor von Eisenhart-Rothe <user@MacBook-Pro-von-User.local>
2025-04-30 15:44:09 +0200

    feat: Linting

--- decoder.py
+++ decoder.py
@@ -2,8 +2,24 @@ from keras import backend as K
 from keras import layers, models, optimizers
 from keras.datasets import mnist, cifar10
 from keras.models import Model, Sequential, load_model
-from keras.layers import Input, Conv2D, Activation, Dense, Dropout, Lambda, Reshape, Concatenate
-from keras.layers import BatchNormalization, MaxPooling2D, Flatten, Conv1D, Deconvolution2D, Conv2DTranspose
+from keras.layers import (
+    Input,
+    Conv2D,
+    Activation,
+    Dense,
+    Dropout,
+    Lambda,
+    Reshape,
+    Concatenate,
+)
+from keras.layers import (
+    BatchNormalization,
+    MaxPooling2D,
+    Flatten,
+    Conv1D,
+    Deconvolution2D,
+    Conv2DTranspose,
+)
 from keras.utils import to_categorical
 from keras.callbacks import Callback, ModelCheckpoint, TensorBoard
 from keras import regularizers
@@ -35,27 +51,59 @@ class args:
     shift_fraction = 0.1
     debug = False
     digit = 5
-    save_dir = '.'
+    save_dir = "."
     t = False
     w = None
     ep_num = 0
 
 
-decoder = models.Sequential(name='decoder')
+decoder = models.Sequential(name="decoder")
 decoder.add(Dense(input_dim=32, activation="relu", output_dim=8 * 8 * 64))
 decoder.add(Reshape((8, 8, 64)))
 decoder.add(BatchNormalization(momentum=0.8))
-decoder.add(layers.Deconvolution2D(256, 3, 3, subsample=(1, 1), border_mode='same', activation="relu"))
-decoder.add(layers.Deconvolution2D(256, 3, 3, subsample=(2, 2), border_mode='same', activation="relu" ))
-decoder.add(layers.Deconvolution2D(256, 3, 3, subsample=(2, 2), border_mode='same', activation="relu"))
-decoder.add(layers.Deconvolution2D(256, 3, 3, subsample=(2, 2), border_mode='same', activation="relu"))
-decoder.add(layers.Deconvolution2D(256, 3, 3, subsample=(2, 2), border_mode='same', activation="relu"))
+decoder.add(
+    layers.Deconvolution2D(
+        256, 3, 3, subsample=(1, 1), border_mode="same", activation="relu"
+    )
+)
+decoder.add(
+    layers.Deconvolution2D(
+        256, 3, 3, subsample=(2, 2), border_mode="same", activation="relu"
+    )
+)
+decoder.add(
+    layers.Deconvolution2D(
+        256, 3, 3, subsample=(2, 2), border_mode="same", activation="relu"
+    )
+)
+decoder.add(
+    layers.Deconvolution2D(
+        256, 3, 3, subsample=(2, 2), border_mode="same", activation="relu"
+    )
+)
+decoder.add(
+    layers.Deconvolution2D(
+        256, 3, 3, subsample=(2, 2), border_mode="same", activation="relu"
+    )
+)
 # decoder.add(layers.Deconvolution2D(256, 3, 3, subsample=(2, 2), border_mode='same', activation="relu"))
 # decoder.add(layers.Deconvolution2D(8, 3, 3, subsample=(2, 2), border_mode='same', activation="relu"))
-decoder.add(layers.Deconvolution2D(16, 3, 3, subsample=(1, 1), border_mode='same', activation="relu"))
-decoder.add(layers.Deconvolution2D(8, 3, 3, subsample=(1, 1), border_mode='same', activation="relu"))
-decoder.add(layers.Deconvolution2D(3, 3, 3, subsample=(1, 1), border_mode='same', activation="relu"))
-decoder.add(layers.Reshape(target_shape=(128, 128, 3), name='out_recon'))
+decoder.add(
+    layers.Deconvolution2D(
+        16, 3, 3, subsample=(1, 1), border_mode="same", activation="relu"
+    )
+)
+decoder.add(
+    layers.Deconvolution2D(
+        8, 3, 3, subsample=(1, 1), border_mode="same", activation="relu"
+    )
+)
+decoder.add(
+    layers.Deconvolution2D(
+        3, 3, 3, subsample=(1, 1), border_mode="same", activation="relu"
+    )
+)
+decoder.add(layers.Reshape(target_shape=(128, 128, 3), name="out_recon"))
 
 decoder.summary()
 
@@ -66,14 +114,18 @@ x256_train = resize(x_train)
 
 vect = np.load("decoder_retrain.npy")
 
-lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * 0.5**(epoch // 5))
+lr_decay = callbacks.LearningRateScheduler(
+    schedule=lambda epoch: args.lr * 0.5 ** (epoch // 5)
+)
 
 parallel_model = multi_gpu_model(decoder, gpus=3)
 
 # compile the model  ssim loss for better reconstruction DSSIMObjective()
-parallel_model.compile(optimizer=optimizers.Adam(lr=args.lr),
-                       loss=['mse'],
-                       metrics={'decoder': "loss"})
+parallel_model.compile(
+    optimizer=optimizers.Adam(lr=args.lr), loss=["mse"], metrics={"decoder": "loss"}
+)
 
-parallel_model.fit(vect,x256_train, batch_size=32, epochs=args.epochs,callbacks=[lr_decay])
-decoder.save(args.save_dir + '/decoder.h5')
+parallel_model.fit(
+    vect, x256_train, batch_size=32, epochs=args.epochs, callbacks=[lr_decay]
+)
+decoder.save(args.save_dir + "/decoder.h5")
--- utils.py
+++ utils.py
@@ -4,18 +4,31 @@ import numpy as np
 
 
 def margin_loss(y_true, y_pred):
-    L = y_true * K.square(K.maximum(0., 0.90 - y_pred)) + 0.5 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.1))
+    L = y_true * K.square(K.maximum(0.0, 0.90 - y_pred)) + 0.5 * (
+        1 - y_true
+    ) * K.square(K.maximum(0.0, y_pred - 0.1))
     return K.mean(K.sum(L, 1))
+
 
 def margin_loss_hard(y_true, y_pred):
-    L = y_true * K.square(K.maximum(0., 0.95 - y_pred)) + 0.8 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.05))
+    L = y_true * K.square(K.maximum(0.0, 0.95 - y_pred)) + 0.8 * (
+        1 - y_true
+    ) * K.square(K.maximum(0.0, y_pred - 0.05))
     return K.mean(K.sum(L, 1))
 
-class CustomModelCheckpoint(callbacks.Callback):
 
-    def __init__(self, model, filepath, monitor='val_loss', verbose=0,
-                 save_best_only=False, save_weights_only=False,
-                 mode='auto', period=1):
+class CustomModelCheckpoint(callbacks.Callback):
+    def __init__(
+        self,
+        model,
+        filepath,
+        monitor="val_loss",
+        verbose=0,
+        save_best_only=False,
+        save_weights_only=False,
+        mode="auto",
+        period=1,
+    ):
         # super(ModelCheckpoint, self).__init__()
         self.monitor = monitor
         self.verbose = verbose
@@ -26,20 +39,22 @@ class CustomModelCheckpoint(callbacks.Callback):
         self.epochs_since_last_save = 0
         self.model_for_saving = model
 
-        if mode not in ['auto', 'min', 'max']:
-            warnings.warn('ModelCheckpoint mode %s is unknown, '
-                          'fallback to auto mode.' % (mode),
-                          RuntimeWarning)
-            mode = 'auto'
+        if mode not in ["auto", "min", "max"]:
+            warnings.warn(
+                "ModelCheckpoint mode %s is unknown, "
+                "fallback to auto mode." % (mode),
+                RuntimeWarning,
+            )
+            mode = "auto"
 
-        if mode == 'min':
+        if mode == "min":
             self.monitor_op = np.less
             self.best = np.Inf
-        elif mode == 'max':
+        elif mode == "max":
             self.monitor_op = np.greater
             self.best = -np.Inf
         else:
-            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+            if "acc" in self.monitor or self.monitor.startswith("fmeasure"):
                 self.monitor_op = np.greater
                 self.best = -np.Inf
             else:
@@ -55,15 +70,25 @@ class CustomModelCheckpoint(callbacks.Callback):
             if self.save_best_only:
                 current = logs.get(self.monitor)
                 if current is None:
-                    warnings.warn('Can save best model only with %s available, '
-                                  'skipping.' % (self.monitor), RuntimeWarning)
+                    warnings.warn(
+                        "Can save best model only with %s available, "
+                        "skipping." % (self.monitor),
+                        RuntimeWarning,
+                    )
                 else:
                     if self.monitor_op(current, self.best):
                         if self.verbose > 0:
-                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
-                                  ' saving model to %s'
-                                  % (epoch + 1, self.monitor, self.best,
-                                     current, filepath))
+                            print(
+                                "\nEpoch %05d: %s improved from %0.5f to %0.5f,"
+                                " saving model to %s"
+                                % (
+                                    epoch + 1,
+                                    self.monitor,
+                                    self.best,
+                                    current,
+                                    filepath,
+                                )
+                            )
                         self.best = current
                         if self.save_weights_only:
                             self.model_for_saving.save_weights(filepath, overwrite=True)
@@ -71,16 +96,14 @@ class CustomModelCheckpoint(callbacks.Callback):
                             self.model_for_saving.save(filepath, overwrite=True)
                     else:
                         if self.verbose > 0:
-                            print('\nEpoch %05d: %s did not improve from %0.5f' %
-                                  (epoch + 1, self.monitor, self.best))
+                            print(
+                                "\nEpoch %05d: %s did not improve from %0.5f"
+                                % (epoch + 1, self.monitor, self.best)
+                            )
             else:
                 if self.verbose > 0:
-                    print('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath))
+                    print("\nEpoch %05d: saving model to %s" % (epoch + 1, filepath))
                 if self.save_weights_only:
                     self.model_for_saving.save_weights(filepath, overwrite=True)
                 else:
                     self.model_for_saving.save(filepath, overwrite=True)
-
-
-                    
-                    
